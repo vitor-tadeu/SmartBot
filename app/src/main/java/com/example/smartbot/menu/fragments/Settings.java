9fdca5545aee11536c0cb373436998efc8eb1dd3
Vitor <vitorarts@hotmail.com>
2019-10-07 22:34:36 -0300

    get uuid

--- app/src/main/java/com/example/smartbot/menu/fragments/Settings.java
+++ app/src/main/java/com/example/smartbot/menu/fragments/Settings.java
@@ -1,19 +1,27 @@
 package com.example.smartbot.menu.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.smartbot.R;
 
 import java.util.Objects;
 
 public class Settings extends Fragment {
-    private Button mTour;
+    private Button mTour, mUUID;
+    private TextView mResposta;
     private View view;
 
     public Settings() {
@@ -29,11 +37,14 @@ public class Settings extends Fragment {
         Objects.requireNonNull(getActivity()).setTitle("Configuração");
         init();
         tour();
+        getUUID();
         return view;
     }
 
     private void init() {
         mTour = view.findViewById(R.id.btnTour);
+        mUUID = view.findViewById(R.id.btnUUID);
+        mResposta = view.findViewById(R.id.txtUUID);
     }
 
     private void tour() {
@@ -47,6 +58,20 @@ public class Settings extends Fragment {
                 FragmentTransaction fragmentTransaction1 = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
                 fragmentTransaction1.replace(R.id.frame, dashboard, "configuracao");
                 fragmentTransaction1.commit();
+            }
+        });
+    }
+
+    private void getUUID() {
+        mUUID.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("HardwareIds")
+            @Override
+            public void onClick(View v) {
+                TelephonyManager tManager = (TelephonyManager) Objects.requireNonNull(getActivity()).getSystemService(Context.TELEPHONY_SERVICE);
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                mResposta.setText(tManager.getDeviceId());
             }
         });
     }
