79d4bc5e503fd091879899c473a156020bf874d5
Vitor <vitorarts@hotmail.com>
2019-10-12 15:17:05 -0300

    constantes

--- app/src/main/java/com/example/smartbot/controller/utils/Constants.java
+++ app/src/main/java/com/example/smartbot/controller/utils/Constants.java
@@ -3,13 +3,12 @@ package com.example.smartbot.controller.utils;
 public class Constants {
     //SplashScreen
     public static final int ERROR_DIALOG_REQUEST = 1;
-    public static final int REQUEST_ACCESS_FINE_LOCATION = 2;
+    public static final int REQUEST_PERMISSIOIN = 2;
     public static final int REQUEST_ENABLE_GPS = 3;
     //Intro
     public static final String INTRO_FIRST = "INTRO_FIRST";
     //Dashboard
     public static final String USUARIO = "USUARIO";
-    public static final String APRESENTACAO_1 = "APRESENTACAO_1";
     public static final int RESULT_SPEECH = 1;
     //Nearby
     public static final String API_KEY = "AIzaSyAiLiJ38jc85o5CSs0wRsKXGpHEjySYO2I";
@@ -17,7 +16,18 @@ public class Constants {
     public static final String POSITION_RAIO = "POSITION_RAIO";
     public static final String FILTRO = "FILTRO";
     public static final String POSITION_FILTRO = "POSITION_FILTRO";
-    public static final String APRESENTACAO_2 = "APRESENTACAO_2";
     //APIClientPlaces
     public static final String API_BASE_URL = "https://maps.googleapis.com/maps/api/";
+    public static final String API_BASE_URL_WEATHER = "http://apiadvisor.climatempo.com.br/";
+    //API OpenWeather
+    public static final String API_KEY_WEATHER = "0283661234635c0bffe28f881395744e";
+    //Passagem de parametro
+    public static final String APRESENTACAO_1 = "APRESENTACAO_1";
+    public static final String APRESENTACAO_2 = "APRESENTACAO_2";
+    public static final String PLACE_ASSISTENTE = "PLACE_ASSISTENTE";
+    public static final String RAIO_ASSISTENTE = "RAIO_ASSISTENTE";
+    public static final String FILTRO_ASSISTENTE = "FILTRO_ASSISTENTE";
+    public static final String CONFIGURACOES = "CONFIGURACOES";
+    public static final String APRESENTACAO = "APRESENTACAO";
+    public static final String TOUR = "TOUR";
 }
