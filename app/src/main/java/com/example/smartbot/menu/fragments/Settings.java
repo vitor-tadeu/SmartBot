fd82e523677dc13ebe48599d53ac661fee802560
Vitor <vitorarts@hotmail.com>
2019-11-01 09:12:41 -0300

    remocao de botao

--- app/src/main/java/com/example/smartbot/menu/fragments/Settings.java
+++ app/src/main/java/com/example/smartbot/menu/fragments/Settings.java
@@ -1,19 +1,12 @@
 package com.example.smartbot.menu.fragments;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.smartbot.R;
 import com.example.smartbot.controller.utils.Constants;
@@ -21,8 +14,7 @@ import com.example.smartbot.controller.utils.Constants;
 import java.util.Objects;
 
 public class Settings extends Fragment {
-    private Button mTour, mUUID;
-    private TextView mResposta;
+    private Button mTour;
     private View view;
 
     public Settings() {
@@ -38,14 +30,11 @@ public class Settings extends Fragment {
         Objects.requireNonNull(getActivity()).setTitle("Configuração");
         init();
         tour();
-        getUUID();
         return view;
     }
 
     private void init() {
         mTour = view.findViewById(R.id.btnTour);
-        mUUID = view.findViewById(R.id.btnUUID);
-        mResposta = view.findViewById(R.id.txtUUID);
     }
 
     private void tour() {
@@ -59,20 +48,6 @@ public class Settings extends Fragment {
                 FragmentTransaction fragmentTransaction1 = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
                 fragmentTransaction1.replace(R.id.frame, dashboard);
                 fragmentTransaction1.commit();
-            }
-        });
-    }
-
-    private void getUUID() {
-        mUUID.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("HardwareIds")
-            @Override
-            public void onClick(View v) {
-                TelephonyManager tManager = (TelephonyManager) Objects.requireNonNull(getActivity()).getSystemService(Context.TELEPHONY_SERVICE);
-                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                mResposta.setText(tManager.getDeviceId());
             }
         });
     }
